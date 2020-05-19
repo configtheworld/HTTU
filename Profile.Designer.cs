15a0f84a8afd75b672f1e2070c78449950bb2913
configtheworld <erkinture@gmail.com>
2020-05-19 16:56:06 +0300

    profil eklendi

--- Profile.Designer.cs
+++ Profile.Designer.cs
@@ -60,7 +60,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(335, 38);
+            this.label1.Location = new System.Drawing.Point(319, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(180, 41);
             this.label1.TabIndex = 15;
