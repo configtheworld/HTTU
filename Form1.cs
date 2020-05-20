b7b970e7be5add61e13037be5fe0606c47befdb4
configtheworld <erkinture@gmail.com>
2020-05-20 13:07:09 +0300

    adsasd

--- Form1.cs
+++ Form1.cs
@@ -61,7 +61,7 @@ namespace HTTU
         {
             SqlConnection con =
                 new SqlConnection(
-                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\doki_\OneDrive\Belgeler\hastadb.mdf;Integrated Security=True;Connect Timeout=30");
+                    @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\berk_\source\repos\HTTU\hastadb.mdf;Integrated Security=True;Connect Timeout=30");
             SqlDataAdapter sqa =
                 new SqlDataAdapter(
                     "Select count(*) From tableSign where Username ='" + nameTB.Text + "'and Password ='" + passwordTB.Text +
