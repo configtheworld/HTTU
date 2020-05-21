723a05b5ba6663a79aa27387ef2f184e47e33d24
dogukan901 <58824480+dogukan901@users.noreply.github.com>
2020-05-21 23:05:40 +0300

    long lat ile

--- UserRegisterForm.cs
+++ UserRegisterForm.cs
@@ -54,7 +54,7 @@ namespace HTTU
         private void RegisterNowButton_Click(object sender, EventArgs e)
         {
             UserAndPassControl user= new UserAndPassControl();
-            user.newUser(nameTB,passwordTB,ConfirmTB,groupBox1);
+            user.newUser(nameTB,passwordTB,Country,Provience,ConfirmTB,Lat,Long,groupBox1);
             this.Close();
         }
 
